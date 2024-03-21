4b296e64a7ff4db7f4ae7df1f808edc7cc61cb3b
Ahmetbasligit config --global user.email ahmet.basli.mec@gmail.com <ahmet.basli@leanscale.com>
2024-03-21 16:24:55 +0300

    Update local strings

--- src/get-dictionaries.ts
+++ src/get-dictionaries.ts
@@ -2,9 +2,11 @@ import "server-only"
 import { Locale } from "../i18n-config"
 
 const dictionaries = {
-  en: () => import("./dictionaries/en.json").then((module) => module.default),
-  ar: () => import("./dictionaries/ar.json").then((module) => module.default),
+  "en-sa": () =>
+    import("./dictionaries/en-sa.json").then((module) => module.default),
+  "ar-sa": () =>
+    import("./dictionaries/ar-sa.json").then((module) => module.default),
 }
 
 export const getDictionary = async (locale: Locale) =>
-  dictionaries[locale]?.() ?? dictionaries.en()
+  dictionaries[locale]?.() ?? dictionaries["en-sa"]()
